e53a2cc58bff481c5ee7a8a00a78edea3ca14917
valentina <valentinasalmon3@gmail.com>
2024-10-03 14:11:31 -0300

    cambios 1

--- esculturas.js
+++ esculturas.js
@@ -2,10 +2,10 @@ const esculturas = [
     {
         id: 1,
         nombre: "Simpleza",
-        foto: "imagenesEsculturas/simpleza.jpg",
+        foto: "https://www.bienaldelchaco.org/2024/wp-content/uploads/2024/07/Bienal-del-Chaco-2024-Argentina-22.jpg",
         autor: "Luis Bernardi",
         pais: "Argentina, Corrientes",
-        descripcion: "Esta obra explora la dualidad de materiales...",
+        descripcion: "Parte de mi escultura se sostiene en la línea, sustentándose en materiales como metal y madera. El color es fundamental en mi trabajo, siempre presente en el recorrido que realizo. Este recorrido se manifiesta en diversas direcciones: subiendo, bajando, doblándose y desapareciendo para volver a aparecer de otra forma. Mi obra busca explorar un mismo lugar repetidamente, haciendo pequeños cambios hasta alcanzar una simpleza mínima casi imperceptible. No tiene un significado explícito, es solo una línea sin intencionalidad, pero su recorrido puede generar interpretaciones. Aspiro a llegar a la simpleza de una flecha o su trayecto.",
         votos: 0,
         valoracion: 0,
         fotoAutor: "https://www.republicadecorrientes.com/content/bucket/4/66054w695h513c.jpg.webp" 
@@ -14,10 +14,10 @@ const esculturas = [
     {
         id: 2,
         nombre: "Multiplicidad",
-        foto: "imagenesEsculturas/multiplicidad.jpg",
+        foto: "https://www.bienaldelchaco.org/2024/wp-content/uploads/2024/07/Bienal-del-Chaco-chile-26.jpg",
         autor: "Alejandro Mardons Gullen",
         pais: "Chile, Valparaíso",
-        descripcion: "La escultura se fundamenta en la línea...",
+        descripcion: "La escultura emplea planos triangulares, color y luz para crear diversas percepciones espaciales. Al observarla desde distintos ángulos, se generan figuras geométricas y volúmenes virtuales. Diseñada para ser explorada, permite a los espectadores situarse dentro de la obra y experimentar un nuevo espacio. Representa la diversidad de individuos en un mismo entorno, donde cada perspectiva es válida. A partir de la individualidad de formas, se crea un todo multidimensional que nos une.",
         votos: 0,
         valoracion: 0,
         fotoAutor: "https://www.bienaldelchaco.org/2024/wp-content/uploads/2024/04/mardones-guillen.fw_-1.png" 
@@ -26,10 +26,10 @@ const esculturas = [
     {
         id: 3,
         nombre: "Mensaje",
-        foto: "imagenesEsculturas/mensaje.jpg",
+        foto: "https://www.bienaldelchaco.org/2024/wp-content/uploads/2024/07/bienal-2024-nueva-zelanda-2.jpg",
         autor: "Anna Kover",
         pais: "Nueva Zelanda, New Plymouth",
-        descripcion: "La obra explora un paisaje onírico...",
+        descripcion: "Una obra sobre el paisaje onírico. Toma la forma abstracta surrealista de un dardo de papel y recuerda al espectador un sentimiento de infancia. Recuerda la sensación de soñar y enviarla como un deseo. La obra utiliza el símbolo del dardo de papel y que recuerda el juego infantil de intentar volar un avión de papel lo más lejos que puedas. Se trata de volar, de esperar y de enviar un deseo a tu yo futuro como la proyección de un sueño. Quizás sea un lugar al que desea viajar, una persona que desea ver… es algo que el espectador debe imaginar. La superficie está facetada, lo que para mí representa algo mágico y alejado de la normalidad de la vida cotidiana.",
         votos: 0,
         valoracion: 0,
         fotoAutor: "https://www.bienaldelchaco.org/2024/wp-content/uploads/2024/04/Nueva-Zelanda-Anna-Korver-Foto.png"
